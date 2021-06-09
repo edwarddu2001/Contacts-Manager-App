3a9907779a33d891c4454b3945685e42b41f646e
Edward Du <edwarddu1234@gmail.com>
2021-06-09 15:33:16 -0400

    Added contactslist entity to domain

---
+++ Domain/Entities/ContactsList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public record Contact { string Name; int PhoneNum; }
+
+    public static class ContactsList
+    {
+        public static List<Contact> Contacts { get; private set; } = new List<Contact>();
+    }
+}
