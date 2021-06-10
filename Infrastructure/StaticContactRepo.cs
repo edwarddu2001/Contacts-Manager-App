2827b07988fb1031ce9e6cc6cfc0d9be9d81ad28
Edward Du <edwarddu1234@gmail.com>
2021-06-10 12:01:24 -0400

    Implement IRepository in StaticContactRepo

--- Infrastructure/StaticContactRepo.cs
+++ Infrastructure/StaticContactRepo.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.Repositories
 
 namespace Infrastructure
 {
-    class StaticContactRepo : IRepository
+    public class StaticContactRepo : IRepository
     {
+        private static List<Contacts> contactsList = new List<Contacts>();
 
+        public List<Contacts> AddContacts(string name, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contacts> RemoveContacts(Contacts contact)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
