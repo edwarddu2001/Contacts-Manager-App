369e85c5466ca9d3bd1ca58c8a33c0fbbad66a31
Prasun Guragain <pguragain@outlook.com>
2021-06-10 11:19:35 -0500

    Query and Handler seperated

---
+++ Application/QueryAndHandle/Handler.cs
@@ -0,0 +1,38 @@
+﻿using Application.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.QueryAndHandle
+{
+    public class Handler : IRequestHandler<Query, List<Contacts>>
+    {
+        private Repository contacts;
+        public Handler(Repository contacts)
+        {
+            this.contacts = contacts;
+        }
+
+        public Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            // how to access name and phoneNumber to give to Repository?
+            // error because it needs to return Task<Response> not List<Contacts>.
+            // After changing that, TRequest and TResponse needs to be changed in IRequestHandler
+            // Maybe make seperate files for cqrs?
+            return Task.FromResult(contacts.AddContacts());
+        }
+
+        public Handler(IDataAcess )
+        {
+
+        }
+        public Task<List<Contacts>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return 
+        }
+    }
+}
---
+++ Application/QueryAndHandle/Query.cs
@@ -0,0 +1,19 @@
+﻿using Application.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.QueryAndHandle
+{
+    public class Query
+    {
+        // automapping between model/viewmodel here probably, by dependency between Application and WebUI
+        public record Command() : IRequest<List<Contacts>>;
+
+        public record Response(string Name, string PhoneNumber);
+    }
+}
