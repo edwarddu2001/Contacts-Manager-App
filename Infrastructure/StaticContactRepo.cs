6d5d84be7d65b402821656033ecef7c32b1373bb
Prasun Guragain <pguragain@outlook.com>
2021-06-10 10:58:52 -0500

    Repository moved to infrastructure

--- Infrastructure/StaticContactRepo.cs
+++ Infrastructure/StaticContactRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,32 @@ namespace Infrastructure
 {
     class StaticContactRepo : IRepository
     {
+        private List<Contacts> contactsList = new();
 
+        // dummy GetContacts(), remove after handeler fixed
+        public List<Contacts> AddContacts()
+        {
+            List<Contacts> contacts = new();
+            return contacts;
+        }
+
+        public List<Contacts> AddContacts(string name, string phoneNumber)
+        {
+            if (!(string.IsNullOrEmpty(name) || string.IsNullOrEmpty(phoneNumber)))
+            {
+                contactsList.Add(new Contacts()
+                {
+                    Name = name,
+                    PhoneNumber = phoneNumber
+                });
+            }
+            return contactsList;
+        }
+
+        public List<Contacts> RemoveContacts(Contacts contact)
+        {
+            contactsList.Remove(contact);
+            return contactsList;
+        }
     }
 }
