24e7cb9b857d6cbb42f9b32d7d2ec79099c6cea5
Edward Du <edwarddu1234@gmail.com>
2021-06-11 17:23:11 -0400

    Update Data Annotation

--- Application/ViewModels/ContactsViewModel.cs
+++ Application/ViewModels/ContactsViewModel.cs
@@ -4,12 +4,13 @@ namespace Application.ViewModels
 {
     public class ContactsViewModel
     {
-        [Required(ErrorMessage = "Name is required")]
-        [StringLength(15, ErrorMessage = "Name is too long")]
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(15, ErrorMessage = "Name is Too Long")]
+        [RegularExpression("[A-Za-z ]+", ErrorMessage = "Name can Only Contain Letters or Spaces")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Phone number is required")]
-        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Phone Number is Required")]
+        [RegularExpression("^[0-9]{3}-[0-9]{3}-[0-9]{4}$", ErrorMessage = "Incorrect Format for Phone Number (ex. ###-###-####)")]
         public string PhoneNum { get; set; }
     }
 }
